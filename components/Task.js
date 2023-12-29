2d230533d06a58b7e9c8a9642962bff040e48f1c
Asim Thaha <iamasimthaha@gmail.com>
2023-12-29 21:49:51 +0530

    completed creating the to do app

--- components/Task.js
+++ components/Task.js
@@ -5,7 +5,7 @@ export default function Task(props) {
   return (
     <View style={styles.item}>
       <View style={styles.itemsLeft}>
-        <TouchableOpacity style={styles.square}></TouchableOpacity>
+        <View style={styles.square}></View>
         <Text style={styles.itemText}>{props.text}</Text>
       </View>
       <View style={styles.circular}></View>
